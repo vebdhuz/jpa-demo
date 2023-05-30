496675a8fdf00f2403730e9659e313ebd1742f38
chenyouxiang <chenyouxiang007@qq.com>
2023-05-30 22:53:21 +0800

    add

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,8 +1,9 @@
 package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
 
+import com.example.demo.provider.UserSql;
 import com.example.demo.entity.User;
 
 import io.lettuce.core.dynamic.annotation.Param;
@@ -11,6 +12,6 @@ import io.lettuce.core.dynamic.annotation.Param;
 @Mapper
 public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User> {
   
-  @Select("select * from user where id = #{id}")
+  @SelectProvider(type=UserSql.class, method="getUser")
   User getUserById(@Param("id") long id);
 }
